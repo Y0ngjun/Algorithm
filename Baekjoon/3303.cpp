52b57ee5604664267b8a1022a998747a2de8d132
SHIN YONG JUN <4rest528@naver.com>
2024-07-10 22:22:33 +0900

    Create 3303.cpp

---
+++ Baekjoon/3303.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int k, q, l, v, kn, p;
+	cin >> k >> q >> l >> v >> kn >> p;
+	cout << 1 - k << ' ' << 1 - q << ' ' << 2 - l << ' ' << 2 - v << ' ' << 2 - kn << ' ' << 8 - p;
+	return 0;
+}
