74c87f3f67dd94441ace563458b05f304b9bf473
SHIN YONG JUN <4rest528@naver.com>
2026-07-05 15:24:03 +0900

    [level 2] Title: 할인 행사, Time: 18.00 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -4,32 +4,32 @@
 
 using namespace std;
 
-int Same(const unordered_map<string, int>& a, unordered_map<string, int>& b)
+int solution(vector<string> want, vector<int> number, vector<string> discount)
 {
-    for (const auto& [s, i] : a)
-        if (b[s] != i)
-            return 0;
-
-    return 1;
-}
-
-int solution(vector<string> want, vector<int> number, vector<string> discount) {
-    unordered_map<string, int> buy;
-    for (int i = 0; i < want.size(); i++)
-        buy[want[i]] = number[i];
-
     unordered_map<string, int> sale;
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 9; i++)
         sale[discount[i]]++;
 
-    int answer = Same(buy, sale);
+    int answer = 0;
 
-    for (int i = 10; i < discount.size(); i++)
+    for (int i = 9; i < discount.size(); i++)
     {
-        sale[discount[i - 10]]--;
         sale[discount[i]]++;
 
-        answer += Same(buy, sale);
+        bool same = true;
+        for (int j = 0; j < want.size(); j++)
+        {
+            if (sale[want[j]] != number[j])
+            {
+                same = false;
+                break;
+            }
+        }
+
+        if (same)
+            answer++;
+
+        sale[discount[i - 9]]--;
     }
 
     return answer;
