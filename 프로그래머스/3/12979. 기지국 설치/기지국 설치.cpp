adb8cd67d60bc1ddeb073a9a270cf7082f118a3c
SHIN YONG JUN <4rest528@naver.com>
2026-07-21 17:00:30 +0900

    [level 3] Title: 기지국 설치, Time: 0.06 ms, Memory: 4.76 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
@@ -4,29 +4,25 @@ using namespace std;
 
 int solution(int n, vector<int> stations, int w)
 {
-    int i = 0; // stations[i]: 가장 가까운 기지국
-    int j = 1; // 현재 위치
-    int k = 0; // 새로운 기지국 개수
-    int width = w + 1 + w;
+    int ans = 0;
+    int prev = 1;
 
-    while (j <= n)
+    for (int station : stations)
     {
-        while (i < stations.size() && stations[i] + w < j)
-        {
-            i++;
-        }
+        int dist = station - w - prev;
 
-        if (i < stations.size() && stations[i] - w <= j && j <= stations[i] + w)
-        {
-            j = stations[i] + w + 1;
-            i++;
-        }
-        else
-        {
-            j += width;
-            k++;
-        }
+        if (dist > 0)
+            ans += (dist + w + w) / (w + w + 1);
+
+        prev = station + w + 1;
     }
 
-    return k;
+    if (prev <= n)
+    {
+        int dist = n - prev + 1;
+
+        ans += (dist + w + w) / (w + w + 1);
+    }
+
+    return ans;
 }
