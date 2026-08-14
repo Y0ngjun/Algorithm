80a63815cd737fd6e630f153a3f43f9c70778b50
SHIN YONG JUN <4rest528@naver.com>
2026-08-14 16:31:17 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 2.24 ms, Memory: 23.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
@@ -0,0 +1,66 @@
+// #include <iostream>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> queue1, vector<int> queue2)
+{
+    int n = queue1.size();
+    int m = queue2.size();
+
+    long long sum = 0LL;
+    long long q1 = 0LL;
+
+    for (int i : queue1)
+    {
+        sum += i;
+        q1 += i;
+    }
+
+    for (int i : queue2)
+    {
+        sum += i;
+    }
+
+    long long half = sum / 2;
+
+    int begin = 0;
+    int end = 0;
+    int answer = 0;
+
+    while (begin < n + m && end < n + m)
+    {
+        // cout << q1 << endl;
+
+        if (q1 > half)
+        {
+            if (begin < n)
+            {
+                q1 -= queue1[begin++];
+            }
+            else
+            {
+                q1 -= queue2[begin++ - n];
+            }
+        }
+        else if (q1 < half)
+        {
+            if (end < m)
+            {
+                q1 += queue2[end++];
+            }
+            else
+            {
+                q1 += queue1[end++ - m];
+            }
+        }
+        else
+        {
+            return answer;
+        }
+
+        answer++;
+    }
+
+    return -1;
+}
