d0d1836dbd80df683133d07ba1750a47f25d2251
SHIN YONG JUN <4rest528@naver.com>
2026-08-24 13:07:09 +0900

    [level 2] Title: 올바른 괄호, Time: 0.37 ms, Memory: 4.53 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s)
+{
+    int cnt = 0;
+
+    for (char c : s)
+    {
+        if (c == '(')
+        {
+            cnt++;
+        }
+        else
+        {
+            if (cnt == 0)
+            {
+                return false;
+            }
+
+            cnt--;
+        }
+    }
+
+    return cnt == 0;
+}
