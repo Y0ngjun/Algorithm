cb0d10c141595a9ce7de36b34f892a5a2c161052
SHIN YONG JUN <4rest528@naver.com>
2026-08-26 11:48:04 +0900

    [level 2] Title: 피로도, Time: 0.04 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,20 +1,15 @@
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int backtracking(int k, const vector<vector<int>>& dungeons, unsigned char bitmask)
+int popcount(int bitmask)
 {
-    int n = dungeons.size();
     int ret = 0;
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i < 256; i <<= 1)
     {
-        if (bitmask & (1 << i) || dungeons[i][0] > k)
-        {
-            continue;
-        }
-
-        ret = max(ret, 1 + backtracking(k - dungeons[i][1], dungeons, bitmask | (1 << i)));
+        if (bitmask & i) ret++;
     }
 
     return ret;
@@ -22,5 +17,31 @@ int backtracking(int k, const vector<vector<int>>& dungeons, unsigned char bitma
 
 int solution(int k, vector<vector<int>> dungeons)
 {
-    return backtracking(k, dungeons, 0);
+    int answer = 0;
+    int n = dungeons.size();
+    int m = (1 << n);
+
+    vector<int> dp(m, -1);
+    dp[0] = k;
+
+    for (int i = 0; i < m; i++)
+    {
+        if (dp[i] == -1) continue;
+
+        for (int j = 0; j < n; j++)
+        {
+            int bit = (1 << j);
+
+            if (i & bit || dungeons[j][0] > dp[i]) continue;
+
+            if (dp[i | bit] == -1 || dp[i | bit] < dp[i] - dungeons[j][1])
+            {
+                dp[i | bit] = dp[i] - dungeons[j][1];
+            }
+        }
+
+        answer = max(answer, popcount(i));
+    }
+
+    return answer;
 }
